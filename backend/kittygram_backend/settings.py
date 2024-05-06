d56e28b40152c7b4d3bc10a8baf2ec8ddba77b74
Haro Mikhaylov <www.haris@bk.ru>
2024-05-06 16:28:12 +0300

    Some fixes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,13 +2,23 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['158.160.88.226', '127.0.0.1', 'localhost', 'kittygram.serveminecraft.net']
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    [
+        '158.160.88.226',
+        '127.0.0.1',
+        'localhost',
+        'kittygram.serveminecraft.net'
+    ]
+)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
